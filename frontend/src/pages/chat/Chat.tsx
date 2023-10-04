6d42aaae36d9a75f2063df7477780ab3f2b0a7dd
stephane-liwerant <135755703+stephane-liwerant@users.noreply.github.com>
2023-10-04 14:26:34 +0000

    Changement du texte à l'ouverture du site

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -548,8 +548,12 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Chatventures Begin</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>
+                                    Welcome to Sociabble AskAI! I'm here to ignite your creativity and streamline content creation, ideas, and training. <br />
+                                    Let's have a fantastic time together! Your data is safe with me - not used for training or stored. <br />
+                                    Enjoy this secure and fun experience!
+                                </h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
